6025ac6d44e6783419bdc03c2b9509ea1cd246b0
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-28 11:01:41 +0100

    .

--- creating_task/urls.py
+++ creating_task/urls.py
@@ -22,8 +22,6 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('', include('thetask.urls')),
     path('', Heyapi),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),  # Inloggning, utloggning etc.
