19fd3844b1b36f6cc65837f713eada51f7a4273d
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-03-04 15:46:44 +0100

    added tokens

--- creating_task/urls.py
+++ creating_task/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('dj-rest-auth/logout/', logout_view, name='logout'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # Inloggning, utloggning etc.
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
- 
+    path('dj-rest-auth/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('dj-rest-auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), 
     
 ]
