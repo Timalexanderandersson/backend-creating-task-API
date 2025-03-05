58606d07fff2f66230d29fa0b802dae852b6089d
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-03-05 11:00:41 +0100

    deleted in settings

--- creating_task/settings.py
+++ creating_task/settings.py
@@ -101,15 +101,6 @@ JWT_AUTH_SAMESITE = 'None'
 
 
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),   
-    'ROTATE_REFRESH_TOKENS': True,    
-    'BLACKLIST_AFTER_ROTATION': True,    
-    'ALGORITHM': 'HS256',    
-    'SIGNING_KEY': 'your-secret-key',
-    'AUTH_HEADER_TYPES': ('Bearer',),
-}
 
 CORS_ALLOW_CREDENTIALS = True
 
