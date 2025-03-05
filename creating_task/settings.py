1e9273f0bd1e5e48e4b59507a1509ed2b5935ef1
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-03-05 14:05:43 +0100

    sessions

--- creating_task/settings.py
+++ creating_task/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        
+        'rest_framework.authentication.SessionAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
