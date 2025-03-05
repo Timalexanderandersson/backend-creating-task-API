6d0667969c30dbcf6b9875d64e9fe7e017803ed4
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-03-05 14:10:07 +0100

    settings deleted for simple-jwt

--- creating_task/settings.py
+++ creating_task/settings.py
@@ -83,12 +83,7 @@ MIDDLEWARE = [
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),   
-    'ROTATE_REFRESH_TOKENS': True,    
-    'BLACKLIST_AFTER_ROTATION': True,    
-    'ALGORITHM': 'HS256',    
-    'SIGNING_KEY': 'your-secret-key',
-    'AUTH_HEADER_TYPES': ('Bearer',),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 
